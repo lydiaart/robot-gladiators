739b89d2e431888a9f229f104376c01daf5e8b1d
Lydia <lydia_art@yahoo.com>
2021-10-10 03:37:24 -0500

    add var & console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+// window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("What is your robot's name?")
+// what is this?
+console.log(playerName)
+console.log("This logs a string, good for leaving yourself a message")
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+// Note the lack of quotation mark around playerName window.alert(playername)
+//this creates a function named "fignt"
+function fight() {
+  window.alert("The fight has begun!")
+}
+// fight();
